1f90f6f74c56149ea8917c97a9c2b172cd278003
dev-assassin <alexandra.uefa@gmail.com>
2018-09-07 17:23:13 +0800

    add routers and articles component

---
+++ src/app/articles/summer-associate/summer-associate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-summer-associate',
+  templateUrl: './summer-associate.component.html',
+  styleUrls: ['./summer-associate.component.css']
+})
+export class SummerAssociateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
