fab815b9268791421226b1c818e247a857cbe0af
dev-assassin <alexandra.uefa@gmail.com>
2018-09-14 07:05:05 +0800

    update footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -12,7 +12,7 @@ export class FooterComponent implements OnInit {
 
   constructor() {
     this.links = [
-      'About Northwest Investor Relations', 'Visual Directory Security Center '
+      'About Northwest', 'Visual Directory', 'Investor Relations', 'Security Center'
     ];
     this.imgs = [
       {
