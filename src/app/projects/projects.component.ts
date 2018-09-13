adb8ce8c8a2d89a66fac57995500a5748e2dc785
dev-assassin <alexandra.uefa@gmail.com>
2018-09-13 15:58:46 +0800

    update project component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -30,27 +30,87 @@ export class ProjectsComponent implements OnInit {
     ];
     this.cards = [
       {
-        title: '2017 Corporate ad campaign',
-        text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
-        images: [
-          'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
-          'assets/images/projects/guy2.png'
+        projects: [
+          {
+            title: '2017 Corporate ad campaign',
+            text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
+            images: [
+              'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
+              'assets/images/projects/guy2.png'
+            ]
+          },
+          {
+            title: 'Banks and the delusion',
+            text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
+            images: [
+              'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
+              'assets/images/projects/guy2.png'
+            ]
+          },
+          {
+            title: 'Branch of the future',
+            text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
+            images: [
+              'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
+              'assets/images/projects/guy2.png'
+            ]
+          }
         ]
       },
       {
-        title: 'Banking For a Better World',
-        text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
-        images: [
-          'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
-          'assets/images/projects/guy2.png'
+        projects: [
+          {
+            title: 'Banking For a Better World',
+            text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
+            images: [
+              'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
+              'assets/images/projects/guy2.png'
+            ]
+          },
+          {
+            title: 'Banking For Solutions',
+            text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
+            images: [
+              'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
+              'assets/images/projects/guy2.png'
+            ]
+          },
+          {
+            title: 'Financial Services Club',
+            text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
+            images: [
+              'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
+              'assets/images/projects/guy2.png'
+            ]
+          }
         ]
       },
       {
-        title: 'Sales and Service Culture',
-        text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
-        images: [
-          'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
-          'assets/images/projects/guy2.png'
+        projects: [
+          {
+            title: 'Sales and Service Culture',
+            text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
+            images: [
+              'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
+              'assets/images/projects/guy2.png'
+            ]
+          },
+          {
+            title: 'Bank’s the Remodel',
+            text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
+            images: [
+              'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
+              'assets/images/projects/guy2.png'
+            ]
+          },
+          {
+            title: 'New iPhone Passbook Wallet',
+            text: 'Get to know our new ad campaign. Because soon, you’ll be seeing it everywhere.',
+            images: [
+              'assets/images/projects/Jane.jpg', 'assets/images/projects/Jennifer.jpg', 'assets/images/projects/guy1.png',
+              'assets/images/projects/guy2.png'
+            ]
+          }
         ]
       }
     ];
