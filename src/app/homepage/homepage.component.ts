104d11ebd59e041889200362a701cbe1f5450dd8
dev-assassin <alexandra.uefa@gmail.com>
2018-09-14 02:19:10 +0800

    update homepage with new design

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -27,15 +27,18 @@ export class HomepageComponent implements OnInit {
   constructor() {
     this.lists = [
       {
+        img: 'assets/images/article1.jpg',
         title: 'Here\'s what\'s new with Beneficial Owner',
         text: 'In 2016, the government\'s Treasury Department\'s Financial Crimes Enforcement Network (FinCEN) issued a rule ' +
           'that will require all banks and credit unions to obtain...'
       },
       {
+        img: 'assets/images/article2.jpg',
         title: 'An update to the CD IMF Playbook.',
         text: 'We\'ve increased deposit rates again on our CD and IMF specials to remain competitive in all our markets.'
       },
       {
+        img: 'assets/images/article3.jpg',
         title: 'Union Community Bank Acquisition Resource Center',
         text: 'We\'re pleased to announce that we will acquire Union Community Bank, headquartered in Lancaster County, Pennsylvania. ' +
           'The conversion is expected to take place the first quarter of 2019.'
@@ -51,7 +54,7 @@ export class HomepageComponent implements OnInit {
         data: 'On Friday, our Oil City office received candy from a customer.'
       },
       {
-        class: 'content',
+        class: '',
         data: 'Manager Kim Reddinger said she was shocked when a longtime customer showed up at the branch bearing ' +
           'gifts in the form of candies for everyone. "John was sure to cover everyone\'s sweet tooth with this assortment!" ' +
           'she said as she smiled and held up the sacks of candy.'
