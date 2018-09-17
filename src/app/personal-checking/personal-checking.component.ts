9b0fd037898a54a7e8a15bce16a1234a06c8c84d
dev-assassin <alexandra.uefa@gmail.com>
2018-09-18 07:34:34 +0800

    update personal page/ add tables

--- src/app/personal-checking/personal-checking.component.ts
+++ src/app/personal-checking/personal-checking.component.ts
@@ -13,6 +13,10 @@ export class PersonalCheckingComponent implements OnInit {
   tools: any;
   tables: any;
   rates: any;
+  lines: any;
+  texts: any;
+  text2: string;
+  text3: string;
 
   constructor(private router: Router) {
     this.text1 = 'At Northwest, we offer a variety of personal checking accounts to suit our customers\' needs.';
@@ -97,6 +101,36 @@ export class PersonalCheckingComponent implements OnInit {
         per: '6.7%'
       }
     ];
+    this.lines = [
+      {
+        line1: '$50 minimum opening balance',
+        line2: '$50 minimum opening balance',
+        line3: '$50 minimum opening balance'
+      },
+      {
+        line1: 'No minimum balance required',
+        line2: '$1,000 minimum average daily balance to avoid a $10 monthly service charge',
+        line3: 'No minimum balance required'
+      },
+      {
+        line1: 'Non-interest bearing',
+        line2: 'Interest bearing',
+        line3: 'Non-interest bearing'
+      }
+    ];
+    this.texts = [
+      'Free online and mobile banking access. Remember—mobile data or text charges may apply.',
+      'Bill Pay', 'Paper or electronic statements', 'Unlimited check and debit card transactions',
+      'Access to 55,000+ free ATMs worldwide, including ATMs in our Allpoint ATM network.', 'Bankline, 24/7 automated telephone banking',
+      'Detect Safe Browsing fraud protection software, which protects customers when they’re logged into Northwest Online Banking',
+      'Northwest Visa debit card', 'Mobile pay—including Apple, Samsung and Google Pay'
+    ];
+    this.text2 = 'Overdraft protection: customers can link their checking account to another Northwest account—like a savings account ' +
+      'or line of credit—to protect themselves from declined purchase which could result in penalties and fees. ' +
+      'Additional checks: fees apply for new check orders. Pro tip—customers can order checks right through Northwest ' +
+      'Online Banking, their local office or by calling the Call Center at 1-877-672-5678.';
+    this.text3 = 'Full courtesy overdraft service: this service gives customers overdraft protection for checks and other ' +
+      'transactions made using their checking account, like automatic bill payments and everyday debit card purchases.';
   }
 
   ngOnInit() {
