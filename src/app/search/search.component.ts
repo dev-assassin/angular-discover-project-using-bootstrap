f8219dffd6472c1cc6f96ea13699134fc7319d2f
dev-assassin <alexandra.uefa@gmail.com>
2018-09-27 07:04:30 +0800

    update search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,7 +8,24 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class SearchComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  manuals: any;
+
+  constructor(private router: Router) {
+    this.manuals = [
+      {
+        img: 'assets/images/search/linklist-manual.svg',
+        text: 'Northwest at Work'
+      },
+      {
+        img: 'assets/images/search/linklist-manual.svg',
+        text: 'Annual Report 2018.pdf'
+      },
+      {
+        img: 'assets/images/search/linklist-manual.svg',
+        text: 'Working with CRM v1.18 02.02.2018'
+      }
+    ];
+  }
 
   ngOnInit() {
     this.router.events.subscribe((evt) => {
